9cfbd38ffbd84e53dcd84189b040ffa1559d7593
Fernando <frnnd.goia97@gmail.com>
2026-02-04 18:37:16 -0300

    feat: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "./components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
+import { Field, FieldLabel, FieldSet } from "./components/ui/field";
+
 function App() {
 	return (
-		<div className="w-screen h-screen bg-[#ddd]">
-			<>teste</>
+		<div className="w-screen h-screen bg-[#ddd] flex items-center justify-center">
+			<Card className="md:w-[500px] bg-white border border-solid border-[#ccc] gap-4">
+				<CardHeader>
+					<CardTitle className="text-xl font-bold">Welcome to CodeLeap network!</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<FieldSet>
+						<Field className="gap-2">
+							<FieldLabel htmlFor="username">Please enter your username</FieldLabel>
+							<Input id="username" type="text" placeholder="John doe" />
+						</Field>
+					</FieldSet>
+				</CardContent>
+				<CardFooter className="flex justify-end">
+					<Button variant={'blue'}>ENTER</Button>
+				</CardFooter>
+			</Card>
 		</div>
 	);
 }
