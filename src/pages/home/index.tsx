515259625fb99d3db7b51f5e67d4de6cdb186598
Fernando <frnnd.goia97@gmail.com>
2026-02-05 18:53:34 -0300

    feat: add title + centered body

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,11 @@
 export default function HomePage() {
     return <div className="w-screen h-screen bg-[#ddd] flex justify-center">
-        <>HomePage</>
+        <div className="bg-white w-[80%] lg:w-[800px]">
+            <div className="bg-[#7695EC] py-7 px-9">
+                <h4 className="text-xl font-semibold text-white">
+                    CodeLeap Network
+                </h4>
+            </div>
+        </div>
     </div>
 }
